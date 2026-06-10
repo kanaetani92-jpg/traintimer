ddeed1c2e09692990d562a742c471eec6cde506c
Kanae Tani <kanaetani92@gmail.com>
2026-06-11 01:40:51 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -20,7 +20,7 @@
   });
   const MAX_TIME_ADDITION_HISTORY = 30;
   const STORAGE_SCHEMA_VERSION = 4;
-  const APP_VERSION = "1.7.0";
+  const APP_VERSION = "1.8.0";
   const SERVICE_WORKER_URL = "sw.js";
   const DEFAULT_USER_PREFERENCES = Object.freeze({
     soundEnabled: true,
