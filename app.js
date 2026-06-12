e8aa352c5cb900ffc69f22cf1e3450d21472320b
Kanae Tani <kanaetani92@gmail.com>
2026-06-12 15:38:55 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -34,7 +34,7 @@
   });
   const MAX_TIME_ADDITION_HISTORY = 30;
   const STORAGE_SCHEMA_VERSION = 11;
-  const APP_VERSION = "1.17.9";
+  const APP_VERSION = "1.18.0";
   const SERVICE_WORKER_URL = "sw.js";
   const DEFAULT_USER_PREFERENCES = Object.freeze({
     soundEnabled: true,
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "train-timer-v1.17.9";
+const CACHE_NAME = "train-timer-v1.18.0";
 const APP_SHELL = [
   "./",
   "./index.html",
